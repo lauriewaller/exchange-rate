93284081b819f545643ca508cdaaf8d189dbdcbe
Laurie Waller <lauriewaller7@gmail.com>
2021-03-25 22:05:42 -0700

    update endpoint type, remote parameter for method

--- src/js/service.js
+++ src/js/service.js
@@ -1,6 +1,6 @@
 export default class ExchangeRate {  
-  static getRate(currency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}`)
+  static getRate() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
