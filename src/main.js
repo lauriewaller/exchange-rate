11aabe41a33989e0ab267bc7a5c41f767ba829d6
Laurie Waller <lauriewaller7@gmail.com>
2021-03-25 21:16:54 -0700

    create working UI for promise

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-//import AddressBook from './js/address-book.js';
+import ExchangeRate from './js/service.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import $ from 'jquery';
+import $ from 'jquery';
 
+function calcRate(response) {
+  if (response) {
+    console.log(response);
+    let dollarAmount = $('#dollar-amount').val();
+    $('.showExchangeRate').text(`${dollarAmount} dollar(s) is ${response.conversion_rate} euros.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
+$('#exchange-button').click(function() {
+  event.preventDefault();
+  let currency = $('#currency-type').val();
+  console.log(currency)
+  ExchangeRate.getRate()
+    .then(function(response) {
+      calcRate(response);
+    });
+});
