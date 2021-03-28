0d933dec2195eb257b78a399ef3e69fc37bc2204
Laurie Waller <lauriewaller7@gmail.com>
2021-03-27 22:22:28 -0700

    update promise logic to handle Error object

--- src/js/service.js
+++ src/js/service.js
@@ -3,12 +3,12 @@ export default class ExchangeRate {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
-          throw Error(response.statusText);
+          throw Error(response.status);
         }
         return response.json();
       })
       .catch(function(error) {
-        return error;
+        return Error(error);
       /* eslint-disable semi */
       })
       /* eslint-enable semi */
